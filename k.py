6f7b7e18f901a1682b32020579f89324864a88b0
ruslanhohol <ruslan27011927@gmail.com>
2018-11-25 01:03:27 +0300

    Add files via upload

---
+++ k.py
@@ -0,0 +1,12 @@
+print("калькулятор")
+a1 = int(input(""))
+a2 = str(input(""))
+a3 = int(input(""))
+if a2 == "+":
+    print(a1 + a3)
+elif a2 == "-":
+    print(a1 - a3)
+elif a2 == "/":
+    print(a1 / a3)
+elif a2 == "*":
+    print(a1 * a3)
